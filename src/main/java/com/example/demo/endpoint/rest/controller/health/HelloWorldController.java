2ddfb3302d5a6297ce504bf0fb9f2371c6a6088f
Andry <140386216+andryraz@users.noreply.github.com>
2025-06-11 19:10:04 +0300

    fix; HelloWorldController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @PojaGenerated
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+  
+    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+    public static final ResponseEntity<String> KO =
+        new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
 
   @GetMapping("/hello")
   public String helloWorld() {
