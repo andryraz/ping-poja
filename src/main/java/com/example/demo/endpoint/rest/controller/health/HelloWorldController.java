e54628d676f89a62c27a67eb1356a95b7b53a168
Andry <140386216+andryraz@users.noreply.github.com>
2025-06-11 15:59:56 +0300

    fix: HelloWorldController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,8 +1,12 @@
-package com.my.company.endpoint.rest.controller.health;
+package com.my.example.demo.endpoint.rest.controller.health;
 
+import com.example.demo.PojaGenerated;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@PojaGenerated
 @RestController
 public class HelloWorldController {
 
