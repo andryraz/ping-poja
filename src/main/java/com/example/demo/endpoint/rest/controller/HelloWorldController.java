552f25bdeff4eff58a9a724627657abd7975881f
andryraz <tiavinarazafindratsimba20@gmail.com>
2025-06-11 21:11:29 +0300

    feat: add asynchronous email sending

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,7 +1,28 @@
 package com.example.demo.endpoint.rest.controller;
 
-import com.example.demo.service.HelloWorldService;
+//import com.example.demo.service.HelloWorldService;
+//import lombok.AllArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@AllArgsConstructor
+//public class HelloWorldController {
+//  private final HelloWorldService service;
+//
+//  @GetMapping("/hello")
+//  public String helloWorld(@RequestParam String name) {
+//    return service.uploadHelloWorldMessage(name);
+//  }
+//}
+
+
+import com.example.demo.endpoint.event.EventProducer;
+import com.example.demo.endpoint.event.model.SendEmailRequested;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +30,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-  private final HelloWorldService service;
+  private final EventProducer<SendEmailRequested> eventProducer;
 
   @GetMapping("/hello")
-  public String helloWorld(@RequestParam String name) {
-    return service.uploadHelloWorldMessage(name);
+  @SneakyThrows
+  public String helloWorld(@RequestParam String to) {
+    var event = SendEmailRequested.builder().to(to).build();
+    eventProducer.accept(List.of(event));
+    return "... world!";
   }
 }
