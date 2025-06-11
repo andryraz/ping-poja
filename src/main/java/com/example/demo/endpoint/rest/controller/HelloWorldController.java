c78465c32df234ed2d6102c924883e80aa382009
andryraz <tiavinarazafindratsimba20@gmail.com>
2025-06-11 19:47:29 +0300

    fix: Appliquer le formatage automatique

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.my.example.demo.endpoint.rest.controller.health;
+package com.example.demo.endpoint.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
